191a86e0b92a7428d8fe1c33cae5805dbb1fc6f1
Antosha9108 <shelkovnikov9108@gmail.com>
2022-05-16 15:52:14 -0400

    added Basic Math Challenge

---
+++ 8kyu/Basic Mathematical Operations.js
@@ -0,0 +1,19 @@
+function basicOp(operation, value1, value2)
+{
+  switch(operation){
+    case '+' :
+        return value1+value2;
+        break;
+    case '-' :
+        return value1-value2;
+        break;
+    case '*' :
+        return value1*value2;
+        break;
+        case '/' :
+        return value1/value2;
+        break;
+
+    
+  }
+}
