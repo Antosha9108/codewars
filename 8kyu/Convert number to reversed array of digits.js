d2d35d8027106bf6e14c6b5dd534b744676a4267
Antosha9108 <shelkovnikov9108@gmail.com>
2022-05-27 15:05:06 -0400

    solved covert num to reversed array of digits kata

--- 8kyu/Convert number to reversed array of digits.js
+++ 8kyu/Convert number to reversed array of digits.js
@@ -1,7 +1,15 @@
-function digitize(n) {
-  
-    arr =  String(n).split("").map((n)=>{
-  return Number(arr).reverse()
-  })
-}
-// THIS KATA IS NOT SOLVED YET!
+function digitize(num) {
+  //function for the Array.from method
+  let myFunc = num => Number(num)
+
+let arr = Array.from(String(num),myFunc)
+
+//reverse the array
+return arr.reverse()
+
+} 
+
+//different solution
+// function digitize(n) {
+//   return String(n).split('').map(Number).reverse()
+// }
