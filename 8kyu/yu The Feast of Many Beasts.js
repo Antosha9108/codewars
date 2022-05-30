2d438f078d181bfbf59e6bdd2d7688e5250552e3
Antosha9108 <shelkovnikov9108@gmail.com>
2022-05-30 13:02:57 -0400

    added feast of many beasts kata

---
+++ 8kyu/yu The Feast of Many Beasts.js
@@ -0,0 +1,9 @@
+function feast(beast, dish) {
+    if (beast[0] ===dish[0] && beast.charAt(beast.length-1) === dish.charAt(dish.length-1)){
+      return true
+    } else {
+      return false
+           }
+    }
+    
+    
