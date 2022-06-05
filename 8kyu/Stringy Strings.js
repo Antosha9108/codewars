e5451bee9be1724a928bb948ca6b1dded6fbf5e1
Antosha9108 <shelkovnikov9108@gmail.com>
2022-06-05 14:14:36 -0400

    added stringy string kata

---
+++ 8kyu/Stringy Strings.js
@@ -0,0 +1,15 @@
+function stringy(size) {
+    for (let i=0;i<size;i++){
+        return i
+    }   
+  }
+
+
+//   a string with size 6 should return :'101010'.
+
+// with size 4 should return : '1010'.
+
+// set up a for loop for the (size) parameter and print char()
+// figure out how to alternate 
+// maybe if indexOf()char 
+// return
