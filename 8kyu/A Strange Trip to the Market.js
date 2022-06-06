6d7a9b4898b55a355d6dd852ff92407b886ad068
Antosha9108 <shelkovnikov9108@gmail.com>
2022-06-06 12:50:35 -0400

    refactored trip to market kata

---
+++ 8kyu/A Strange Trip to the Market.js
@@ -0,0 +1,9 @@
+// function isLockNessMonster(s) {
+//     if (s.includes('tree fiddy')|| s.includes('3.50')){
+//       return true
+//     } else {
+//       return false
+//     }
+//   }
+
+let isLockNessMonster = s => (s.includes('tree fiddy')|| s.includes('3.50'))? true : false
