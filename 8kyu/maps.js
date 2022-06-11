03e2944037afce2eac8dd64b2541cb761ddef983
Antosha9108 <shelkovnikov9108@gmail.com>
2022-06-11 14:56:20 -0400

    added maps kata

---
+++ 8kyu/maps.js
@@ -0,0 +1,14 @@
+function maps(x){
+    map1 = x.map(element => element*2)
+      return map1
+    } 
+
+
+    const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+        if (distanceToPump => Number(mpg*fuelLeft)){
+          return true
+        }else {
+          return false
+        }
+        // TODO
+      };
