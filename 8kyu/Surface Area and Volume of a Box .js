63dfa62f9d3b9928248c36a2ef7ffcb731055002
Antosha9108 <shelkovnikov9108@gmail.com>
2022-06-13 13:48:59 -0400

    added surface and volume of the box kata

---
+++ 8kyu/Surface Area and Volume of a Box .js
@@ -0,0 +1,7 @@
+function getSize(width, height, depth){
+    let area = 2*((height *width) + (height * depth) + (width * depth));
+    let volume =width*height*depth
+    return arr =[area,volume]
+  }
+  
+  
