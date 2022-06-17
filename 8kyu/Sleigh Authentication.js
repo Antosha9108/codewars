afb837bf3f92c76ba4bd45131c936c8d1b1495f7
Antosha9108 <shelkovnikov9108@gmail.com>
2022-06-17 12:04:39 -0400

    added sleigh kata

---
+++ 8kyu/Sleigh Authentication.js
@@ -0,0 +1,9 @@
+function Sleigh() {}
+
+Sleigh.prototype.authenticate = function(name, password) {
+  if (name ==="Santa Claus" && password === 'Ho Ho Ho!'){
+    return true
+  } else {
+    return false
+  }
+};
