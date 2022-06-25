70774b66f2c8c776afdb9cb6bdc9cbfbb62928c0
Antosha9108 <shelkovnikov9108@gmail.com>
2022-06-25 13:22:54 -0400

    added rooms kata

---
+++ Grasshopper - Create the rooms.js
@@ -0,0 +1,18 @@
+
+var rooms = {
+ 'room1' : {
+  'name': 'entrance',
+  'desription': "find the way in",
+  'completed': true,
+},
+  'room2' : {
+  'name': 'cellar',
+  'desription': 'find the wine to escape',
+  'completed': false,
+  },
+  'room3' : {
+  'name': 'greenhouse',
+  'desription': 'find the way out',
+  'completed': true,
+  },
+}
