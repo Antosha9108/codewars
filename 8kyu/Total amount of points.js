5b695cf9d8d186ed6c6cf4feb3fd8bbf0eb68e68
Antosha9108 <shelkovnikov9108@gmail.com>
2022-08-12 14:20:32 -0400

    added total points kata

---
+++ 8kyu/Total amount of points.js
@@ -0,0 +1,23 @@
+function points(games) {
+    // P an array of string
+    // R an integer having all the score together
+    // E ['1:0','2:0','3:0','4:0','2:1','3:1','4:1','3:2','4:2','4:3'] =>30
+    // P first we declare an empty array where we would add the point.
+       let totalPoints =[]
+    //       second we need to determine how to get two values (x/y) from every element of the array.
+    //       then we put in conditional logic to see if x >y ,x<y, x==y output the point accordingly 
+    //       and push it to the totalPoints array.
+          
+        for (let i=0; i<games.length;i++){   
+            if (games[i][0] >games[i][2]){
+            totalPoints.push(3)
+            } else if(games[i][0] <games[i][2]){
+            totalPoints.push (0)
+            } else if (games[i][0] == games[i][2]){
+            totalPoints.push (1)
+            }
+      
+  //   we return the reduced array to see the total number
+    }
+    return totalPoints.reduce((a,b) => a+b)
+}
