56ad3d7a33afb891d37d83d63fbbb1f4bb3d5a2f
Antosha9108 <shelkovnikov9108@gmail.com>
2022-08-15 16:46:51 -0400

    added Wilson Primes kata

--- 8kyu/Total amount of points.js
+++ 8kyu/Total amount of points.js
@@ -21,3 +21,19 @@ function points(games) {
     }
     return totalPoints.reduce((a,b) => a+b)
 }
+
+
+
+
+
+// function points(games) {
+//     let total = 0;
+//     games.map(game => {
+//       if (game[0] === game[2]) {
+//         total += 1;
+//       } else if (game[0] > game[2]) {
+//         total += 3;
+//       }
+//     });
+//     return total;
+//   }
---
+++ 8kyu/Wilson Primes.js
@@ -0,0 +1,15 @@
+let amIWilson = p => p==5||p==13 || p==563? true : false
+  // check if prime is Wilson
+//   P integer
+//   R boolean
+//   E 5 => true, 9 => false
+//   P the only known wilson primes numbers are 5,13,563
+//   we need to see if p is equal to one of these numbers and return true if yes and false otherwise
+  
+//   if (p==5||p==13 || p==563){
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+// }
