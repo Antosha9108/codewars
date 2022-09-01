7b67192f72ee6d68e0cddebb23093d4ce0fd4ef7
Antosha9108 <shelkovnikov9108@gmail.com>
2022-09-01 15:47:52 -0400

    adde sort and star kata

---
+++ 8kyu/Sort and Star.js
@@ -0,0 +1,12 @@
+function twoSort(s) {
+let word = s.sort()[0]
+return word.split('').join('***')
+}
+
+// P an array of strings
+// R a string with *** between each letter
+// E "bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n' )
+// P first we need to sort the array alphabetically and pull out the first
+//   word. after that we need to input *** between every letter
+
+
