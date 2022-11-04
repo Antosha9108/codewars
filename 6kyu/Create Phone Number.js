d459dca9c6ed0a1354542d4bf5aa828933161d5c
Antosha9108 <shelkovnikov9108@gmail.com>
2022-11-04 15:33:56 -0400

    added create phone num kata

--- 6kyu/Create Phone Number.js
+++ 6kyu/Create Phone Number.js
@@ -1,3 +1,5 @@
 function createPhoneNumber(numbers){
   return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6,10).join('')}`
 }
+
+// we can use string literal and slice the needed numbers in
