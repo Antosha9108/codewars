fe1b6e946f06e8c3585f17af4d6e4d8112004bb7
Antosha9108 <shelkovnikov9108@gmail.com>
2022-12-24 13:16:50 -0500

    refactored vowel remover kata

--- 8kyu/Vowel remover.js
+++ 8kyu/Vowel remover.js
@@ -1,3 +1,24 @@
-function shortcut (string) {
-  return string.replace(/[a,e,i,o,u]/g,'');
+function shortcut(string) {
+  return string.replace(/[a,e,i,o,u]/g, '');
 }
+
+
+
+
+function shortcut(string) {
+  let vowels = 'aeiou'
+  return string.split('').filter(element => !vowels.includes(element)).join('');
+}
+
+
+// string
+
+// string with vowels filtered out
+
+// hello => hll
+
+// first we need to declare a variable containing the vowels.
+
+// then we split the string into an array and filter out the vowels.
+
+// lastly we join it back into a string
