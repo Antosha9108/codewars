cfcb7f491c3e4ecfbe914ab32377ba259b0303eb
Antosha9108 <shelkovnikov9108@gmail.com>
2023-01-11 12:10:40 -0500

    added insert dashes kata

--- 7kyu/insert dashes.js
+++ 7kyu/insert dashes.js
@@ -18,8 +18,7 @@ function insertDash(num) {
 // E (454793),'4547-9-3')
 // P first we need to find consecutive odd numbers. we can do it with a for loop and modulus %2==0
 //     on two consecutive numbers()
-//     if condition is true
-//     we push num[i] and we push('-') to the array
+//     if condition is true we push num[i] and we push('-') to the array
 //     else we push num[i]
 
 //     lastly we need to get rid of the last dash with .pop() method
