bd0f8d3b498c32b0624cdb2282d2ef454616e2c7
Antosha9108 <shelkovnikov9108@gmail.com>
2023-01-18 13:09:42 -0500

    refactored sheep kata

--- 8kyu/If you can't sleep, just count sheep!!.js
+++ 8kyu/If you can't sleep, just count sheep!!.js
@@ -1,11 +1,19 @@
-var countSheep = function (num){
-    let result = "";
-    if (num==0){
-      return ''
-    } else{
-    for (let i=1; i<=num;i++){
-      result += i +' sheep...'
+var countSheep = function (num) {
+  let result = "";
+  if (num == 0) {
+    return ''
+  } else {
+    for (let i = 1; i <= num; i++) {
+      result += i + ' sheep...'
     }
     return result
   }
+}
+
+var countSheep = function (num) {
+  let arr = [];
+  for (let i = 1; i <= num; i++) {
+    arr.push(i)
+  }
+  return arr.map(element => `${element} sheep...`).join('')
 }
