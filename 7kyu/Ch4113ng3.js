ab9f0c67c5aca548b8205837a2eda2d3b591eaec
Antosha9108 <shelkovnikov9108@gmail.com>
2023-04-11 12:46:49 -0400

    added Ch4113ng3 kata

---
+++ 7kyu/Ch4113ng3.js
@@ -0,0 +1,7 @@
+function nerdify(txt) {
+    return txt.replaceAll('a', '4')
+        .replaceAll('A', '4')
+        .replaceAll('e', '3')
+        .replaceAll('E', '3')
+        .replaceAll('l', '1')
+}
