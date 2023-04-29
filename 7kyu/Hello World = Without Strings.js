b385c0312fa75b9d8f413415e0b505139a5e1d6e
Antosha9108 <shelkovnikov9108@gmail.com>
2023-04-29 10:45:10 -0400

    added shorter solution

--- 7kyu/Hello World = Without Strings.js
+++ 7kyu/Hello World = Without Strings.js
@@ -4,3 +4,9 @@ const helloWorld = () => {
 
 
 };
+
+
+const helloWorld = () => {
+    let myArray = [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33];
+    return String.fromCharCode(...myArray)
+};
