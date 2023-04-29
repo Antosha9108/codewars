62ae0a803c3689ac8ae001ef3a4cb9174695602e
Antosha9108 <shelkovnikov9108@gmail.com>
2023-04-29 10:42:59 -0400

    hello world w/o strings

---
+++ 7kyu/Hello World = Without Strings.js
@@ -0,0 +1,6 @@
+const helloWorld = () => {
+    let myArray = [72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33];
+    return myArray.map(el => String.fromCharCode(el)).reduce((acc, c) => acc + c)
+
+
+};
