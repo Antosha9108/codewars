f187278a1eba7cae820ee5496d42a8f0097c0b5a
Antosha9108 <shelkovnikov9108@gmail.com>
2023-04-30 13:10:40 -0400

    added welcome kata

---
+++ 8kyu/Welcome!.js
@@ -0,0 +1,36 @@
+function greet(language) {
+
+    let dataBase = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    }
+
+    for (let key in dataBase) {
+        if (key === language) {
+            return dataBase[key];
+        }
+    }
+    return 'Welcome'
+
+    // P: a string
+    // R: return a string with the greeting
+    // E: english: 'Welcome', czech: 'Vitejte',
+    // P: we need to compare language parameter to all the languages and then return the according
+    // greeting with for in loop and key
+
+}
