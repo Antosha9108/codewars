8e6f3d26f1c3a2253b4c40cfc4f7986b81b65685
Antosha9108 <shelkovnikov9108@gmail.com>
2023-05-02 12:11:03 -0400

    added Remove First and Last Character Part Two

---
+++ 8kyu/Remove First and Last Character Part Two .js
@@ -0,0 +1,12 @@
+function array(str) {
+    console.log(str)
+    let arr = str.split(',')
+    let arr2 = str.split(',').slice(1, -1).filter(el => el != ',').join(' ')
+    if (arr.length <= 2) {
+        return null
+    } else {
+        return arr2
+    }
+
+
+}
