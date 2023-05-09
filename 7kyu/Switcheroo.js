7a1ce4a77517fb3aa822a95ac7ae787585396574
Antosha9108 <shelkovnikov9108@gmail.com>
2023-05-09 12:49:28 -0400

    added map and ternary solution

--- 7kyu/Switcheroo.js
+++ 7kyu/Switcheroo.js
@@ -1,3 +1,9 @@
 function switcheroo(x) {
     return x.replaceAll('a', 'x').replaceAll('b', 'y').replaceAll('x', 'b').replaceAll('y', 'a')
 }
+
+
+
+function switcheroo(x) {
+    return x.split('').map(char => char === 'a' ? 'b' : (char === 'b' ? 'a' : 'c')).join("")
+}
