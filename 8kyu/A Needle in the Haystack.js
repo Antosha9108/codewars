00dfcf8304711b53d6e71c7f0b90ec4f5d06a5e7
Antosha9108 <shelkovnikov9108@gmail.com>
2023-06-27 16:39:35 -0400

    refactoreda needle kata

--- 8kyu/A Needle in the Haystack.js
+++ 8kyu/A Needle in the Haystack.js
@@ -1,10 +1,14 @@
 function findNeedle(haystack) {
-    let found = haystack.findIndex(element => element ==='needle');
+    let found = haystack.findIndex(element => element === 'needle');
     if (found in haystack) {
-        return(`found the needle at position ${found}`)
-    }  else {
+        return (`found the needle at position ${found}`)
+    } else {
         return ("Your function didn't return anything")
     }
-    
-    }
-  
+
+}
+
+function findNeedle(haystack) {
+
+    return `found the needle at position ${haystack.indexOf('needle')}`
+}
