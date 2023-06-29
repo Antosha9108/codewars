9c4297f749efac8ed6402936052bf8e14cbd8ace
Antosha9108 <shelkovnikov9108@gmail.com>
2023-06-29 15:25:50 -0400

    refactored fake bin kata

--- 8kyu/Fake binary.js
+++ 8kyu/Fake binary.js
@@ -1,11 +1,18 @@
-function fakeBin(arr){
-let newArr =arr.split('').map(binary).join('')
-function binary(value){
-    if(value<5){
-         return value = 0
-    } else{
-         return value =  1
-    }
+function fakeBin(arr) {
+     let newArr = arr.split('').map(binary).join('')
+     function binary(value) {
+          if (value < 5) {
+               return value = 0
+          } else {
+               return value = 1
+          }
+     }
+     return newArr
 }
-return newArr
+
+
+function fakeBin(x) {
+     let bin = x.split('').map(num => Number(num))
+     return bin.map(num => num < 5 ? 0 : 1).join('')
+
 }
