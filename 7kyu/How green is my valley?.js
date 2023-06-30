8e6519644ef63621ba06f7750df8356192a5a4a2
Antosha9108 <shelkovnikov9108@gmail.com>
2023-06-30 13:09:17 -0400

    refactored green valley kata

--- 7kyu/How green is my valley?.js
+++ 7kyu/How green is my valley?.js
@@ -9,3 +9,13 @@ function makeValley(arr) {
     }
     return res
 }
+
+
+function makeValley(arr) {
+    var leftWing = []
+    var rightWing = []
+    arr.sort((a, b) => b - a)
+        .forEach((el, i) => i % 2 == 0 ? leftWing.push(el) : rightWing.unshift(el))
+
+    return [...leftWing, ...rightWing]
+}
