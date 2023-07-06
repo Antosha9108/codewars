25766b718600778af7ba1448cd5049987467ca1f
Antosha9108 <shelkovnikov9108@gmail.com>
2023-07-06 15:11:18 -0400

    refactored simple remove dups

--- 7kyu/Simple remove duplicates.js
+++ 7kyu/Simple remove duplicates.js
@@ -5,3 +5,9 @@ function solve(arr) {
 
 // that way we start filtering from the end of the array. and then reverse it back to get filtered
 // array in the orignal order
+
+// alternatively we can use the lastIndexOf
+
+function solve(arr) {
+    return arr.filter((val, i) => arr.lastIndexOf(val) == i);
+}
