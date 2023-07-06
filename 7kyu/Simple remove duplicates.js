9eee7d54cf65cd63c4e7e726f546782ff112fe17
Antosha9108 <shelkovnikov9108@gmail.com>
2023-07-06 15:07:35 -0400

    added simple remove dups

---
+++ 7kyu/Simple remove duplicates.js
@@ -0,0 +1,7 @@
+function solve(arr) {
+    return arr.reverse().filter((a, b) => arr.indexOf(a) === b).reverse()
+}
+// We can try to reverse the array and then remove the duplicates and then reverse it back
+
+// that way we start filtering from the end of the array. and then reverse it back to get filtered
+// array in the orignal order
