a9419134378cd2f542e29f817dc2013e940dae46
Antosha9108 <shelkovnikov9108@gmail.com>
2023-10-01 17:54:00 -0400

    fixed stringy string kata

--- 8kyu/Stringy Strings.js
+++ 8kyu/Stringy Strings.js
@@ -1,15 +1,8 @@
 function stringy(size) {
-    for (let i=0;i<size;i++){
-        return i
-    }   
+  let newString = '';
+  for (let i = 0; i < size; i++) {
+    newString += '10'
   }
-
-
-//   a string with size 6 should return :'101010'.
-
-// with size 4 should return : '1010'.
-
-// set up a for loop for the (size) parameter and print char()
-// figure out how to alternate 
-// maybe if indexOf()char 
-// return
+  console.log(newString)
+  return newString.slice(0, size)
+}
