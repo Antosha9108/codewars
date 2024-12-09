03512de7be4fcdd2c396b456cfc283d9f8818876
Anton <shelkovnikov9108@gmail.com>
2024-12-09 17:41:49 -0500

    added new solution

--- 8kyu/pick a set of first elements.js
+++ 8kyu/pick a set of first elements.js
@@ -1,9 +1,21 @@
-function first(arr, n) {
-    if (n===0){
-      return []
-    } else if (n>1){
-      return arr.slice(0,n)
-    }else {
-      return arr.slice(0,1)
-    }
-  }
+// function first(arr, n) {
+//     if (n===0){
+//       return []
+//     } else if (n>1){
+//       return arr.slice(0,n)
+//     }else {
+//       return arr.slice(0,1)
+//     }
+//   }
+
+function first(arr, n = 1) {
+  return arr.slice(0, n)
+}
+
+// P array and int
+// R array of elements up to n
+// E  const arr = ['a', 'b', 'c', 'd', 'e'];
+//     assert.sameOrderedMembers(first([...arr]   ),      ['a']);
+//     assert.sameOrderedMembers(first([...arr], 2), ['a', 'b']);
+// P we can slice the array starting from 0 to n
+//   to address the edge case when n is undefined we can pass a default value of 1 to n
