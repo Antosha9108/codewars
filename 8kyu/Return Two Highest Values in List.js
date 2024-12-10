0c2e0ef8d844d550ca952f586de6140fc08788a0
Anton <shelkovnikov9108@gmail.com>
2024-12-10 12:49:07 -0500

    added solution to two highest values

--- 8kyu/Return Two Highest Values in List.js
+++ 8kyu/Return Two Highest Values in List.js
@@ -1,3 +1,24 @@
 function twoHighest(arr) {
-    return arr.sort((a,b)=>b-a).filter((a, b) => arr.indexOf(a) === b).slice(0,2)
-  }
+  return arr.sort((a, b) => b - a).filter((a, b) => arr.indexOf(a) === b).slice(0, 2)
+}
+
+
+
+function twoHighest(arr) {
+
+  console.log(arr)
+  let sortedArray = arr.sort((a, b) => b - a)
+  let removedDupes = [...new Set(sortedArray)]
+
+  return removedDupes.slice(0, 2)
+
+}
+
+// P array of integers
+// R 2 highest numbers (edge case if array.length is less than 2 return the first one)
+// E 15,20,20,17 => 20,17
+// P
+
+// first we need to sort the array in reverse order.
+// after that we can remove the duplicates
+//lastly we can slice the array that will return first 2 values.
