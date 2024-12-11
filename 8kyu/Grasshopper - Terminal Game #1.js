a7bd69cae36587c1d72d7bcb8a850d4434dd3076
Anton <shelkovnikov9108@gmail.com>
2024-12-11 17:31:43 -0500

    added new solution to hq9+

--- 8kyu/Grasshopper - Terminal Game #1.js
+++ 8kyu/Grasshopper - Terminal Game #1.js
@@ -1,11 +1,20 @@
-function Hero (name) {
+function Hero(name) {
   this.name = name,
-  this.position = '00',
-  this.health = 100,
-  this.damage = 5,
-  this.experience = 0;
-  
+    this.position = '00',
+    this.health = 100,
+    this.damage = 5,
+    this.experience = 0;
+
   if (this.name == undefined) {
     this.name = "Hero"
   }
+}
+
+
+function Hero(name = 'Hero') {
+  this.name = name;
+  this.position = '00';
+  this.health = 100;
+  this.damage = 5;
+  this.experience = 0;
 }
